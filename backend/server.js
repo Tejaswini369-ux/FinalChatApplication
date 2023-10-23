09c44e89135850fa72da7df3550720fd690d0620
Tejaswini369-ux <115396014+Tejaswini369-ux@users.noreply.github.com>
2023-10-23 19:19:08 +0530

    removed unnecessary comment lines

--- backend/server.js
+++ backend/server.js
@@ -21,23 +21,6 @@ app.use(express.json());
 app.use('/api/user',userRoutes)
 app.use('/api/chat',chatRoutes)
 app.use('/api/message',messageRoutes)
-
-// -----------------deployement-----------------
-
-const __dirname1=path.resolve();
-if(process.env.NODE_ENV==='production'){
-   app.use(express.static(path.join(__dirname1,'/frontend/build')))
-   
-}else{
-  
- app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-
-}
-
-// ------------deployement-----------
-
 // app.use(notFound)
 // app.use(errorHandler)
 
