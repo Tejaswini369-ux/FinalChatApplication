24719189298bbabc0a5da1c434808b52ccf771c0
Tejaswini369-ux <varretejaswini@gmail.com>
2024-03-24 23:10:15 +0530

    updated package.json

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,10 +1,13 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
-const isProduction = true;
+const isProduction = process.env.NODE_ENV == 'production';
 
-module.exports = {
+
+const config = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -17,6 +20,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: 'index.html',
         }),
+
         // Add your plugins here
         // Learn more about plugins from https://webpack.js.org/configuration/plugins/
     ],
@@ -30,24 +34,62 @@ module.exports = {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
                 type: 'asset',
             },
+
             // Add your rules for custom modules here
             // Learn more about loaders from https://webpack.js.org/loaders/
         ],
     },
-    resolve: {
-        fallback: {
-            zlib: require.resolve('browserify-zlib'),
-            querystring: require.resolve('querystring-es3'),
-            path: require.resolve('path-browserify'),
-            crypto: false,
-            stream: false,
-        },
-    },
 };
 
-if (isProduction) {
-    module.exports.mode = 'production';
-    module.exports.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
-} else {
-    module.exports.mode = 'development';
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+        
+        
+        config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+        
+    } else {
+        config.mode = 'development';
+    }
+    return config;
+};
+
+module.exports={
+    resolve: {
+    fallback: {
+      zlib: require.resolve('browserify-zlib')
+    }
+  }
 }
+module.exports={
+    resolve: {
+    fallback: {
+      querystring: require.resolve('querystring-es3')
+    }
+  }
+}
+module.exports={
+    resolve: {
+    fallback: {
+      path: require.resolve('path-browserify')
+    }
+  }
+}
+module.exports = {
+  // Other configuration options...
+
+  resolve: {
+    fallback: {
+      crypto: false
+    }
+  }
+};
+module.exports = {
+  // Other configuration options...
+
+  resolve: {
+    fallback: {
+      stream: false
+    }
+  }
+};
