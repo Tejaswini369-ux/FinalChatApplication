75d8d7c7fe9456f3c5476aba3318a039325d02dd
Tejaswini369-ux <varretejaswini@gmail.com>
2024-03-24 11:44:29 +0530

    updated backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,15 @@
-const mongoose = require("mongoose")
-const colors=require('colors')
-const connectDB=async()=>{
-    try{
-        const conn=await mongoose.connect('mongodb+srv://me:Tejaswini@cluster0.462lywd.mongodb.net/?retryWrites=true&w=majority',{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify:true,
+const mongoose = require("mongoose");
+const colors = require('colors');
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect('mongodb+srv://tejaswini:tejaswini@cluster0.drfriin.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
         });
-        console.log(`MONGODB connected: ${conn.connection.host}`.cyan.underline)
-    }catch(error){
-       console.log(`Error : ${error.message}`.red.bold);
-       process.exit();
+        console.log(`MONGODB connected: ${conn.connection.host}`.cyan.underline);
+    } catch (error) {
+        console.log(`Error : ${error.message}`.red.bold);
+        process.exit(1);
     }
 };
-module.exports=connectDB;
+
+module.exports = connectDB;
--- backend/controllers/messagecontrollers.js
+++ backend/controllers/messagecontrollers.js
@@ -19,17 +19,16 @@ const { content, chatId } = req.body;
 
   try {
     var message = await Message.create(newMessage);
-
-    message = await message.populate("sender", "name pic").execPopulate();
-    message = await message.populate("chat").execPopulate();
+    message = await message.populate("sender", "name pic");
+    message = await message.populate("chat");
     message = await User.populate(message, {
       path: "chat.users",
       select: "name pic email",
     });
 
-    await Chat.findByIdAndUpdate(req.body.chatId, { latestMessage: message });
+    const newch = await Chat.findByIdAndUpdate(req.body.chatId, { latestMessage: message });
 
-    res.json(message);
+    return res.status(200).json(message);
   } catch (error) {
     res.status(400);
     throw new Error(error.message);
