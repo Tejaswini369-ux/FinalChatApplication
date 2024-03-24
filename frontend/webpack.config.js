b0d2619794afc17789ca4d377992da173a78093a
Tejaswini369-ux <varretejaswini@gmail.com>
2024-03-24 23:35:45 +0530

    updated package.json

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,13 +1,10 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
-const isProduction = false;
+const isProduction = process.env.NODE_ENV == 'production';
 
-
-const config = {
+module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -20,9 +17,6 @@ const config = {
         new HtmlWebpackPlugin({
             template: 'index.html',
         }),
-
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
     ],
     module: {
         rules: [
@@ -31,65 +25,27 @@ const config = {
                 loader: 'babel-loader',
             },
             {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
                 type: 'asset',
             },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
         ],
+    },
+    resolve: {
+        fallback: {
+            zlib: require.resolve('browserify-zlib'),
+            querystring: require.resolve('querystring-es3'),
+            path: require.resolve('path-browserify'),
+        },
     },
 };
 
-module.exports = () => {
-    if (isProduction) {
-        config.mode = 'production';
-        
-        
-        config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
-        
-    } else {
-        config.mode = 'development';
-    }
-    return config;
-};
-
-module.exports={
-    resolve: {
-    fallback: {
-      zlib: require.resolve('browserify-zlib')
-    }
-  }
+if (isProduction) {
+    module.exports.mode = 'production';
+    module.exports.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+} else {
+    module.exports.mode = 'development';
 }
-module.exports={
-    resolve: {
-    fallback: {
-      querystring: require.resolve('querystring-es3')
-    }
-  }
-}
-module.exports={
-    resolve: {
-    fallback: {
-      path: require.resolve('path-browserify')
-    }
-  }
-}
-module.exports = {
-  // Other configuration options...
-
-  resolve: {
-    fallback: {
-      crypto: false
-    }
-  }
-};
-module.exports = {
-  // Other configuration options...
-
-  resolve: {
-    fallback: {
-      stream: false
-    }
-  }
-};
