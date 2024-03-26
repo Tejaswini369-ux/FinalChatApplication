320479232eab2840abb2fb71ad590f1915b0ae65
Tejaswini369-ux <115396014+Tejaswini369-ux@users.noreply.github.com>
2024-03-26 10:05:42 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ const server= app.listen(PORT,console.log(`Server Started on PORT ${PORT}` ));
 const io=require("socket.io")(server,{
   pingTimeout:60000,
   cors:{
-    origin:"http://localhost:3000"
+    origin:["https://chatapplication-isf8.onrender.com", "http://localhost:3000","https://final-chat-application.vercel.app"]
   },
 })
 
